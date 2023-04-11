44d524e31bce3ee7df91de96d1c6c12f6fbb1b5f
Macailh <salvadorenriquegb@gmail.com>
2023-04-11 10:54:24 -0600

    Add passport

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express = require("express");
-const app = express();
+const passport = require("passport");
+require("./auth")(passport);
 
+const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
   //req - request
   //res - response
   res.status(200).send("Hello");
+});
+
+app.post("/login", (req, res) => {
+  res
+    .status(200)
+    .json({
+      token:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.350o18fZPeOi3tEGEac6U4UzuB_k-FuZeVQvzf369IQ",
+    });
 });
 
 app.get("/team", (req, res) => {
---
+++ auth.js
@@ -0,0 +1,16 @@
+const JwtStrategy = require("passport-jwt").Strategy,
+  ExtractJwt = require("passport-jwt").ExtractJwt;
+
+module.exports = (passport) => {
+  const opts = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme("jwt"),
+    secretOrKey: "qwertyr",
+  };
+
+  passport.use(
+    new JwtStrategy(opts, (decoded, done) => {
+      console.log("decoded jwt", decoded);
+      return done(null, false);
+    })
+  );
+};
